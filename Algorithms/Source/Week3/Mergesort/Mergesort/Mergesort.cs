b6cad7ce6aa1135944c84efe9f726825da7ac9b1
Ivan Kononenko <ivan.s.kononenko.homemail@gmail.com>
2014-03-24 09:28:32 +0200

    C# Mergesort

---
+++ Algorithms/Source/Week3/Mergesort/Mergesort/Mergesort.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mergesort
+{
+    public interface ISort<T>
+    {
+        void Sort(IEnumerable<T> elements);
+        void Sort(ref T[] elements, IComparer<T> comparer);
+
+    }
+
+    public interface ISort
+    {
+        void Sort(IComparable[] elements);
+    }
+
+
+    public class Mergesort<T> : ISort<T>
+    {
+        private IComparer<T> _comparer;
+        public void Sort<T>(IEnumerable<T> elements)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Sort(ref T[] elements, IComparer<T> comparer)
+        {
+            _comparer = comparer;
+            T[] aux = new T[elements.Count()];
+            Sort(elements, aux, 0, elements.Count() - 1);
+            elements = aux;
+        }
+
+        private void Sort(T[] a, T[] aux, int lo, int hi)
+        {
+            if (hi <= lo)
+                return;
+            int mid = lo + (hi - lo) / 2;
+            Sort(a, aux, lo, mid);
+            Sort(a, aux, mid + 1, hi);
+
+            Merge(a, aux, lo, mid, hi);
+        }
+
+        private void Merge(T[] a, T[] aux, int lo, int mid, int hi)
+        {
+            int i = lo, j = mid + 1;
+            for (int k = lo; k <= hi; k++)
+            {
+                if (i > mid)
+                    aux[k] = a[j++];
+                else if (j > hi)
+                    aux[k] = a[i++];
+                else if (_comparer.Compare(a[i], a[j]) < 0)
+                    aux[k] = a[i++];
+                else
+                    aux[k] = a[j++];
+            }
+
+        }
+
+
+        public void Sort(IEnumerable<T> elements)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+    public class Mergesort : ISort
+    {
+
+        public void Sort(int[] a)
+        {
+            int[] aux = new int[a.Length];
+            Sort(a, aux, 0, a.Length - 1);
+            Console.Read();
+        }
+
+        private void Sort(int[] a, int[] aux, int lo, int hi)
+        {
+            if (hi <= lo)
+                return;
+            int mid = lo + (hi - lo) / 2;
+            Sort(a, aux, lo, mid);
+            Sort(a, aux, mid + 1, hi);
+            Merge(a, aux, lo, mid, hi);
+        }
+        private void Merge(int[] a, int[] aux, int lo, int mid, int hi)
+        {
+            for (int k = lo; k <= hi; k++)
+                aux[k] = a[k];
+            int i = lo, j = mid + 1;
+            for (int k = lo; k <= hi; k++)
+            {
+                if (i > mid)
+                    a[k] = aux[j++];
+                else if (j > hi)
+                    a[k] = aux[i++];
+                else if (aux[i] < aux[j])
+                    a[k] = aux[i++];
+                else
+                    a[k] = aux[j++];
+            }
+
+            Console.Write(lo + " " + hi + " " + "|");
+            foreach (int el in a)
+                Console.Write(" " + el);
+
+            Console.WriteLine();
+        }
+
+        public void Sort(IComparable[] elements)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Algorithms/Source/Week3/Mergesort/Mergesort/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mergesort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 97, 13, 83, 12 };
+
+            Car[] cars = {
+                new Car{Name= "Honda", Price=1200},
+                new Car{Name= "A", Price=1400},
+                new Car{Name= "A", Price=500},
+                new Car{Name= "B2", Price=1600}
+            };
+            Mergesort<Car> mergeSort = new Mergesort<Car>();
+            mergeSort.Sort(ref cars, new CarPriceComparer());
+            mergeSort.Sort(ref cars, new CarNameComparer());
+            foreach (var car in cars)
+            {
+                Console.WriteLine("{0} \t {1}", car.Name, car.Price);
+            }
+            //mergeSort.BottomUp_MergeSort(arr);
+
+        }
+    }
+
+    public class CarPriceComparer : IComparer<Car>
+    {
+        public int Compare(Car x, Car y)
+        {
+            if (x.Price == y.Price)
+                return 0;
+            else if (x.Price > y.Price)
+                return 1;
+            else
+                return -1;
+
+        }
+    }
+
+    public class CarNameComparer : IComparer<Car> {
+        public int Compare(Car x, Car y)
+        {
+            return string.Compare(x.Name, y.Name);
+        }
+
+    }
+
+
+    public class Car
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+
+
+
+    public class Mergesort1
+    {
+
+        public void BottomUp_MergeSort(int[] a)
+        {
+            int N = a.Length;
+            int[] aux = new int[N];
+            for (int i = 1; i < N; i = i + i)
+                for (int j = 0; j < N - i; j += i + i)
+                {
+                    int lo = j;
+                    int mid = j + i - 1;
+                    int hi = Math.Min(j + i + i - 1, N - 1);
+                    //Merge(a, aux, j, mid, hi);
+                }
+        }
+    }
+
+    public class A : IComparable, IComparable<int>, IEquatable<int> //, IFormattable, IConvertible, , 
+    {
+        public int CompareTo(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CompareTo(int other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Equals(int other)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+    public class Employee : IComparer, IComparable
+    {
+        public int Compare(object x, object y)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public int CompareTo(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
